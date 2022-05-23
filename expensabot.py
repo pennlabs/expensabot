e32cac8d8f17dcbb2e4b2081faae252a5cb95bad
cphalen <cphalen@seas.upenn.edu>
2022-05-23 14:00:19 -0500

    Initial rejection email functionality

--- expensabot.py
+++ expensabot.py
@@ -7,6 +7,7 @@ from datetime import date
 from email.message import EmailMessage
 from functools import wraps
 from io import BytesIO
+from schema import Schema
 
 import requests
 from docx import Document
@@ -26,6 +27,10 @@ host = os.environ.get("SMTP_HOST")
 port = os.environ.get("SMTP_PORT")
 username = os.environ.get("SMTP_USERNAME")
 password = os.environ.get("SMTP_PASSWORD")
+
+rejection_schema = Schema(
+    {"recipients": [{"email": str, "name": str}], "directors": str}
+)
 
 
 def require_apikey(view_function):
@@ -48,7 +53,15 @@ def index():
 @app.route("/submit", methods=["GET", "POST"])
 @require_apikey
 def submit():
-    fields = ["name", "email", "supplier", "date", "amount", "description", "receipt_id"]
+    fields = [
+        "name",
+        "email",
+        "supplier",
+        "date",
+        "amount",
+        "description",
+        "receipt_id",
+    ]
     if request.method == "GET":
         s = '<form method="post">'
         s = s + "".join([f'{f}: <input name="{f}" /> <br />' for f in fields])
@@ -60,6 +73,18 @@ def submit():
         doc_stream, receipt_stream = generate_report(request.form)
         send_report(doc_stream, receipt_stream, request.form)
         return "message sent!"
+
+
+@app.route("/rejection", methods=["POST"])
+@require_apikey
+def rejection():
+    form = request.json
+    if not rejection_schema.validate(form):
+        return Response(status=400)
+    for recipient in form["recipients"]:
+        print(f"Sending to: {recipient['name']}")
+        send_rejection(recipient["email"], recipient["name"], form["directors"])
+    return "rejection email(s) sent!"
 
 
 def send_report(doc_stream, receipt_stream, data):
@@ -87,7 +112,9 @@ def send_report(doc_stream, receipt_stream, data):
     r, t = receipt_stream
     if r is not None:
         maint, subt = t.split("/")
-        msg.add_attachment(r.read(), maintype=maint, subtype=subt, filename=f"Receipt.{subt}")
+        msg.add_attachment(
+            r.read(), maintype=maint, subtype=subt, filename=f"Receipt.{subt}"
+        )
 
     with smtplib.SMTP(host, 587) as server:
         server.login(username, password)
@@ -126,6 +153,44 @@ def generate_report(data):
         return d, (i, mtype)
     except:  # noqa
         return d, (None, None)
+
+
+def send_rejection(recipient_email, recipient_name, from_name):
+    msg = EmailMessage()
+
+    msg["Subject"] = "[Penn Labs] Thank you for your application"
+
+    msg["From"] = from_email
+    msg["To"] = recipient_email
+    msg["Cc"] = copy_emails
+
+    msg.set_content(
+        "\n".join(
+            [
+                f"Hi {recipient_name},",
+                ""
+                "Thank you for taking the time to apply to Penn Labs! This semester's recruitment process was difficult, and unfortunately we were not able to accept your application at this time.",
+                "",
+                "We truly appreciate the time you took to chat with us and complete the technical. Our decision is by no means a reflection of your ability, but a reality of how the club's personnel needs vary by semester, and how we staff our teams.",
+                "",
+                "We know that rejections from clubs can be disheartening, but we sincerely hope that you continue building and looking for ways to improve the Penn experience. Many of our members were not accepted on their initial application attempt, but they continued to learn, and are now some of our most valuable contributors. On that note, we have a few bits of advice to help grow the skills and passions you very clearly have:",
+                "\t1. Identify a problem at Penn, then go out and solve it: There are so many problems at Penn you can solve and we want to see the amazing things you can do even outside of Penn Labs! We also have APIs for you to use!",
+                "\t2. Immerse yourself in the world of products: There are a lot of great resources out there to dive deeper into product development and design. Product Hunt and Dribbble are great places to find inspiration - find things you like and think about how they're built.",
+                "3. Build something: Seriously, anything. Even if it's not super cool at first, you'll learn an incredible amount in a short amount of time. Many of our developers started school with little experience, but over the course of a semester or even a break, picked up a new language and started building non-stop.",
+                "",
+                "If you have any further questions about Labs or your application, please don't hesitate to reach out! Again, thank you so much for taking the time to apply and chat with us."
+                "",
+                "Best,",
+                f"{from_name}",
+                "Penn Labs Co-Directors",
+            ]
+        )
+    )
+
+    with smtplib.SMTP(host, 587) as server:
+        server.login(username, password)
+        server.send_message(msg)
+        return True
 
 
 if __name__ == "__main__":
